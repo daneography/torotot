149eeb5195a3cce8c0c2c9eeb128fb4772008618
Dane Acena <acena.dane@gmail.com>
2020-06-30 21:40:24 -0400

    Initial Commit

---
+++ torotot/torotot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  torotot
+//
+//  Created by Dane Acena on 6/30/20.
+//  Copyright © 2020 daneography. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ torotot/torototTests/torototTests.swift
@@ -0,0 +1,34 @@
+//
+//  torototTests.swift
+//  torototTests
+//
+//  Created by Dane Acena on 6/30/20.
+//  Copyright © 2020 daneography. All rights reserved.
+//
+
+import XCTest
+@testable import torotot
+
+class torototTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
